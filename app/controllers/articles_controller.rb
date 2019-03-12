4e2e1732ea5113504393850a809337ce5e769cb2
Thomas <tkociolek@live.fr>
2019-03-12 14:05:23 +0000

    action show et new complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,17 @@ class ArticlesController < ApplicationController
     end
     
     def create
-        #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        if @article.save
+            flash[:notice] = "Article bel et bien crée"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        @article = Article.find(params[:id])
     end
     
     private
@@ -16,4 +23,5 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)
     end
+    
 end
