61715504c7d4b9b1bc2c719de83a30664b6e7ec4
Thomas <tkociolek@live.fr>
2019-03-12 14:59:55 +0000

    articles edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -14,9 +18,20 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def update 
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article bel et bien modifié"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
     def show
         @article = Article.find(params[:id])
     end
+    
     
     private
     
