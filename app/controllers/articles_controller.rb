4b3fa9f8b59e2f095a76ac044e140669d432d907
Thomas <tkociolek@live.fr>
2019-03-14 13:10:50 +0000

    flash messages and validation errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article bel et bien crée"
+            flash[:success] = "Article bel et bien crée"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
     
     def update 
         if @article.update(article_params)
-            flash[:notice] = "Article bel et bien modifié"
+            flash[:success] = "Article bel et bien modifié"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
     
     def destroy
         @article.destroy
-        flash[:notice] = "Article supprimé"
+        flash[:danger] = "Article supprimé"
         redirect_to articles_path
     end
     
