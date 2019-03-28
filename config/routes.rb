ce32d49a4e2f4905e91cfcb479f469ae30bab7dc
Thomas <tkociolek@live.fr>
2019-03-28 15:15:12 +0000

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   resources :articles
   
   root 'pages#home'
+  
   get 'about', to: 'pages#about'
   
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
